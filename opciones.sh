20b4bec9a04a4dafcaefe706ea2b8939f51b712f
Nicope18 <nicopellegrini2013@gmail.com>
2024-08-04 22:45:02 -0300

    Ejercicio 3

---
+++ opciones.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "ingresar opcion:"
+    echo " Opción 1"
+    echo " Opción 2"
+    echo " Opción 3"
+else
+    echo "La opción ingresada es: $1"
+fi
+
